9b577aadb020f8298bcfedc1b9fc98d09643dc83
Tim Schindler <xh3b4sd@gmail.com>
2021-01-14 23:52:03 +0100

    add first conformance test (#1)

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/venturemark/fmz
+
+go 1.15
+
+require (
+	github.com/venturemark/apigengo v0.0.0-20210114083259-2aab618959c2
+	github.com/xh3b4sd/tracer v0.3.1
+	google.golang.org/grpc v1.35.0
+)
---
+++ pkg/client/client.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"github.com/venturemark/apigengo/pkg/pbf/audience"
+	"github.com/xh3b4sd/tracer"
+	"google.golang.org/grpc"
+)
+
+type Config struct {
+}
+
+type Client struct {
+	connection *grpc.ClientConn
+
+	audience audience.APIClient
+}
+
+func New(c Config) (*Client, error) {
+	var err error
+
+	var con *grpc.ClientConn
+	{
+		con, err = grpc.Dial("127.0.0.1:7777", grpc.WithInsecure())
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
+	var aud audience.APIClient
+	{
+		aud = audience.NewAPIClient(con)
+	}
+
+	cli := &Client{
+		connection: con,
+
+		audience: aud,
+	}
+
+	return cli, nil
+}
+
+func (c *Client) Connection() *grpc.ClientConn {
+	return c.connection
+}
+
+func (c *Client) Audience() audience.APIClient {
+	return c.audience
+}
