e8c26450b6e0c7465ea87ec98826ab57aac3ade0
Tim Schindler <xh3b4sd@gmail.com>
2021-01-16 13:38:06 +0100

    extent audience conformance tests for complete lifecycle

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/venturemark/fmz
 go 1.15
 
 require (
-	github.com/venturemark/apigengo v0.0.0-20210114083259-2aab618959c2
+	github.com/venturemark/apigengo v0.0.0-20210115214846-44f82a16198f
 	github.com/xh3b4sd/tracer v0.3.1
 	google.golang.org/grpc v1.35.0
 )
--- tst/audience_test.go
+++ tst/audience_test.go
@@ -25,6 +25,7 @@ func Test_Audience_001(t *testing.T) {
 		defer cli.Connection().Close()
 	}
 
+	var aid string
 	{
 		i := &audience.CreateI{
 			Obj: &audience.CreateI_Obj{
@@ -47,10 +48,12 @@ func Test_Audience_001(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		_, ok := o.Obj.Metadata["audience.venturemark.co/id"]
+		s, ok := o.Obj.Metadata["audience.venturemark.co/id"]
 		if !ok {
 			t.Fatal("audience ID must not be empty")
 		}
+
+		aid = s
 	}
 
 	{
@@ -73,6 +76,114 @@ func Test_Audience_001(t *testing.T) {
 		_, err := cli.Audience().Create(context.Background(), i)
 		if err == nil {
 			t.Fatal("audience name must be unique")
+		}
+	}
+
+	{
+		i := &audience.DeleteI{
+			Obj: &audience.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     aid,
+					"organization.venturemark.co/id": "org",
+					"user.venturemark.co/id":         "usr",
+				},
+			},
+		}
+
+		o, err := cli.Audience().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/status"]
+		if !ok {
+			t.Fatal("audience ID must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("audience status must be deleted")
+		}
+	}
+
+	{
+		i := &audience.CreateI{
+			Obj: &audience.CreateI_Obj{
+				Metadata: map[string]string{
+					"organization.venturemark.co/id": "org",
+					"user.venturemark.co/id":         "usr",
+				},
+				Property: &audience.CreateI_Obj_Property{
+					Name: "Employees",
+					User: []string{
+						"xh3b4sd",
+						"marcoelli",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Create(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/id"]
+		if !ok {
+			t.Fatal("audience ID must not be empty")
+		}
+
+		aid = s
+	}
+
+	{
+		i := &audience.SearchI{
+			Obj: []*audience.SearchI_Obj{
+				{
+					Metadata: map[string]string{
+						"organization.venturemark.co/id": "org",
+						"user.venturemark.co/id":         "usr",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Search(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(o.Obj) != 1 {
+			t.Fatal("there must be one audience")
+		}
+
+		if o.Obj[0].Property.Name != "Employees" {
+			t.Fatal("audience name must be Employees")
+		}
+	}
+
+	{
+		i := &audience.DeleteI{
+			Obj: &audience.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     aid,
+					"organization.venturemark.co/id": "org",
+					"user.venturemark.co/id":         "usr",
+				},
+			},
+		}
+
+		o, err := cli.Audience().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/status"]
+		if !ok {
+			t.Fatal("audience ID must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("audience status must be deleted")
 		}
 	}
 }
