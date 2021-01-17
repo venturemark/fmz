e88d18be1b4f4a47abca2a2dfbca5ea182dd7238
Tim Schindler <xh3b4sd@gmail.com>
2021-01-17 23:15:41 +0100

    restructure existing conformance tests

--- tst/texupd_test.go
+++ tst/texupd_test.go
@@ -115,118 +115,6 @@ func Test_TexUpd_Lifecycle(t *testing.T) {
 	}
 
 	{
-		i := &texupd.DeleteI{
-			Obj: &texupd.DeleteI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     "1",
-					"organization.venturemark.co/id": "1",
-					"timeline.venturemark.co/id":     tid,
-					"update.venturemark.co/id":       ui1,
-					"user.venturemark.co/id":         "1",
-				},
-			},
-		}
-
-		o, err := cli.TexUpd().Delete(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["update.venturemark.co/status"]
-		if !ok {
-			t.Fatal("texupd status must not be empty")
-		}
-
-		if s != "deleted" {
-			t.Fatal("texupd status must be deleted")
-		}
-	}
-
-	{
-		i := &texupd.DeleteI{
-			Obj: &texupd.DeleteI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     "1",
-					"organization.venturemark.co/id": "1",
-					"timeline.venturemark.co/id":     tid,
-					"update.venturemark.co/id":       ui2,
-					"user.venturemark.co/id":         "1",
-				},
-			},
-		}
-
-		o, err := cli.TexUpd().Delete(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["update.venturemark.co/status"]
-		if !ok {
-			t.Fatal("texupd status must not be empty")
-		}
-
-		if s != "deleted" {
-			t.Fatal("texupd status must be deleted")
-		}
-	}
-
-	{
-		i := &texupd.CreateI{
-			Obj: &texupd.CreateI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     "1",
-					"organization.venturemark.co/id": "1",
-					"timeline.venturemark.co/id":     tid,
-					"user.venturemark.co/id":         "1",
-				},
-				Property: &texupd.CreateI_Obj_Property{
-					Text: "Lorem ipsum 1",
-				},
-			},
-		}
-
-		o, err := cli.TexUpd().Create(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["update.venturemark.co/id"]
-		if !ok {
-			t.Fatal("texupd ID must not be empty")
-		}
-
-		ui1 = s
-	}
-
-	{
-		i := &texupd.CreateI{
-			Obj: &texupd.CreateI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     "1",
-					"organization.venturemark.co/id": "1",
-					"timeline.venturemark.co/id":     tid,
-					"user.venturemark.co/id":         "1",
-				},
-				Property: &texupd.CreateI_Obj_Property{
-					Text: "Lorem ipsum 2",
-				},
-			},
-		}
-
-		o, err := cli.TexUpd().Create(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["update.venturemark.co/id"]
-		if !ok {
-			t.Fatal("texupd ID must not be empty")
-		}
-
-		ui2 = s
-	}
-
-	{
 		i := &update.SearchI{
 			Obj: []*update.SearchI_Obj{
 				{
@@ -338,6 +226,67 @@ func Test_TexUpd_Lifecycle(t *testing.T) {
 
 		if s != "deleted" {
 			t.Fatal("timeline status must be deleted")
+		}
+	}
+
+	{
+		i := &update.SearchI{
+			Obj: []*update.SearchI_Obj{
+				{
+					Metadata: map[string]string{
+						"audience.venturemark.co/id":     "1",
+						"organization.venturemark.co/id": "1",
+						"timeline.venturemark.co/id":     tid,
+						"user.venturemark.co/id":         "1",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Update().Search(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(o.Obj) != 0 {
+			t.Fatal("there must be zero updates")
+		}
+	}
+}
+
+func Test_TexUpd_No_Timeline(t *testing.T) {
+	var err error
+
+	var cli *client.Client
+	{
+		c := client.Config{}
+
+		cli, err = client.New(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer cli.Connection().Close()
+	}
+
+	{
+		i := &texupd.CreateI{
+			Obj: &texupd.CreateI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     "1",
+					"organization.venturemark.co/id": "1",
+					"timeline.venturemark.co/id":     "1",
+					"user.venturemark.co/id":         "1",
+				},
+				Property: &texupd.CreateI_Obj_Property{
+					Text: "Lorem ipsum 1",
+				},
+			},
+		}
+
+		_, err := cli.TexUpd().Create(context.Background(), i)
+		if err == nil {
+			t.Fatal("update must not be created without timeline")
 		}
 	}
 }
--- tst/timeline_test.go
+++ tst/timeline_test.go
@@ -26,6 +26,185 @@ func Test_Timeline_Lifecycle(t *testing.T) {
 		defer cli.Connection().Close()
 	}
 
+	var ti1 string
+	{
+		i := &timeline.CreateI{
+			Obj: &timeline.CreateI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     "1",
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+				Property: &timeline.CreateI_Obj_Property{
+					Name: "Marketing Campaign",
+				},
+			},
+		}
+
+		o, err := cli.Timeline().Create(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["timeline.venturemark.co/id"]
+		if !ok {
+			t.Fatal("timeline ID must not be empty")
+		}
+
+		ti1 = s
+	}
+
+	var ti2 string
+	{
+		i := &timeline.CreateI{
+			Obj: &timeline.CreateI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     "1",
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+				Property: &timeline.CreateI_Obj_Property{
+					Name: "Internal Project",
+				},
+			},
+		}
+
+		o, err := cli.Timeline().Create(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["timeline.venturemark.co/id"]
+		if !ok {
+			t.Fatal("timeline ID must not be empty")
+		}
+
+		ti2 = s
+	}
+
+	{
+		i := &timeline.SearchI{
+			Obj: []*timeline.SearchI_Obj{
+				{
+					Metadata: map[string]string{
+						"audience.venturemark.co/id":     "1",
+						"organization.venturemark.co/id": "1",
+						"user.venturemark.co/id":         "1",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Timeline().Search(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(o.Obj) != 2 {
+			t.Fatal("there must be two timelines")
+		}
+
+		if o.Obj[0].Property.Name != "Internal Project" {
+			t.Fatal("timeline name must be Internal Project")
+		}
+		if o.Obj[1].Property.Name != "Marketing Campaign" {
+			t.Fatal("timeline name must be Marketing Campaign")
+		}
+	}
+
+	{
+		i := &timeline.DeleteI{
+			Obj: &timeline.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     "1",
+					"timeline.venturemark.co/id":     ti1,
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+			},
+		}
+
+		o, err := cli.Timeline().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["timeline.venturemark.co/status"]
+		if !ok {
+			t.Fatal("timeline status must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("timeline status must be deleted")
+		}
+	}
+
+	{
+		i := &timeline.DeleteI{
+			Obj: &timeline.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     "1",
+					"timeline.venturemark.co/id":     ti2,
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+			},
+		}
+
+		o, err := cli.Timeline().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["timeline.venturemark.co/status"]
+		if !ok {
+			t.Fatal("timeline status must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("timeline status must be deleted")
+		}
+	}
+
+	{
+		i := &timeline.SearchI{
+			Obj: []*timeline.SearchI_Obj{
+				{
+					Metadata: map[string]string{
+						"audience.venturemark.co/id":     "1",
+						"organization.venturemark.co/id": "1",
+						"user.venturemark.co/id":         "1",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Timeline().Search(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(o.Obj) != 0 {
+			t.Fatal("there must be zero timelines")
+		}
+	}
+}
+
+func Test_Timeline_Unique_Name(t *testing.T) {
+	var err error
+
+	var cli *client.Client
+	{
+		c := client.Config{}
+
+		cli, err = client.New(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer cli.Connection().Close()
+	}
+
 	var tid string
 	{
 		i := &timeline.CreateI{
@@ -71,87 +250,6 @@ func Test_Timeline_Lifecycle(t *testing.T) {
 		_, err := cli.Timeline().Create(context.Background(), i)
 		if err == nil {
 			t.Fatal("timeline name must be unique")
-		}
-	}
-
-	{
-		i := &timeline.DeleteI{
-			Obj: &timeline.DeleteI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     "1",
-					"organization.venturemark.co/id": "1",
-					"timeline.venturemark.co/id":     tid,
-					"user.venturemark.co/id":         "1",
-				},
-			},
-		}
-
-		o, err := cli.Timeline().Delete(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["timeline.venturemark.co/status"]
-		if !ok {
-			t.Fatal("timeline status must not be empty")
-		}
-
-		if s != "deleted" {
-			t.Fatal("timeline status must be deleted")
-		}
-	}
-
-	{
-		i := &timeline.CreateI{
-			Obj: &timeline.CreateI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     "1",
-					"organization.venturemark.co/id": "1",
-					"user.venturemark.co/id":         "1",
-				},
-				Property: &timeline.CreateI_Obj_Property{
-					Name: "Marketing Campaign",
-				},
-			},
-		}
-
-		o, err := cli.Timeline().Create(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["timeline.venturemark.co/id"]
-		if !ok {
-			t.Fatal("timeline ID must not be empty")
-		}
-
-		tid = s
-	}
-
-	{
-		i := &timeline.SearchI{
-			Obj: []*timeline.SearchI_Obj{
-				{
-					Metadata: map[string]string{
-						"audience.venturemark.co/id":     "1",
-						"organization.venturemark.co/id": "1",
-						"user.venturemark.co/id":         "1",
-					},
-				},
-			},
-		}
-
-		o, err := cli.Timeline().Search(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if len(o.Obj) != 1 {
-			t.Fatal("there must be one timeline")
-		}
-
-		if o.Obj[0].Property.Name != "Marketing Campaign" {
-			t.Fatal("timeline name must be Marketing Campaign")
 		}
 	}
 
