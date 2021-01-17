32c17cb0024e04f299acbe5e57d098b68db1197b
Tim Schindler <xh3b4sd@gmail.com>
2021-01-17 23:20:26 +0100

    restructure existing conformance tests (#6)

--- tst/audience_test.go
+++ tst/audience_test.go
@@ -26,6 +26,187 @@ func Test_Audience_Lifecycle(t *testing.T) {
 		defer cli.Connection().Close()
 	}
 
+	var ai1 string
+	{
+		i := &audience.CreateI{
+			Obj: &audience.CreateI_Obj{
+				Metadata: map[string]string{
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+				Property: &audience.CreateI_Obj_Property{
+					Name: "Employees",
+					User: []string{
+						"xh3b4sd",
+						"marcoelli",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Create(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/id"]
+		if !ok {
+			t.Fatal("audience ID must not be empty")
+		}
+
+		ai1 = s
+	}
+
+	var ai2 string
+	{
+		i := &audience.CreateI{
+			Obj: &audience.CreateI_Obj{
+				Metadata: map[string]string{
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+				Property: &audience.CreateI_Obj_Property{
+					Name: "Vendors",
+					User: []string{
+						"xh3b4sd",
+						"marcoelli",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Create(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/id"]
+		if !ok {
+			t.Fatal("audience ID must not be empty")
+		}
+
+		ai2 = s
+	}
+
+	{
+		i := &audience.SearchI{
+			Obj: []*audience.SearchI_Obj{
+				{
+					Metadata: map[string]string{
+						"organization.venturemark.co/id": "1",
+						"user.venturemark.co/id":         "1",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Search(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(o.Obj) != 2 {
+			t.Fatal("there must be two audiences")
+		}
+
+		if o.Obj[0].Property.Name != "Vendors" {
+			t.Fatal("audience name must be Vendors")
+		}
+		if o.Obj[1].Property.Name != "Employees" {
+			t.Fatal("audience name must be Employees")
+		}
+	}
+
+	{
+		i := &audience.DeleteI{
+			Obj: &audience.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     ai1,
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+			},
+		}
+
+		o, err := cli.Audience().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/status"]
+		if !ok {
+			t.Fatal("audience status must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("audience status must be deleted")
+		}
+	}
+
+	{
+		i := &audience.DeleteI{
+			Obj: &audience.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     ai2,
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+			},
+		}
+
+		o, err := cli.Audience().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/status"]
+		if !ok {
+			t.Fatal("audience status must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("audience status must be deleted")
+		}
+	}
+
+	{
+		i := &audience.SearchI{
+			Obj: []*audience.SearchI_Obj{
+				{
+					Metadata: map[string]string{
+						"organization.venturemark.co/id": "1",
+						"user.venturemark.co/id":         "1",
+					},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Search(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(o.Obj) != 0 {
+			t.Fatal("there must be zero audiences")
+		}
+	}
+}
+
+func Test_Audience_Unique_Name(t *testing.T) {
+	var err error
+
+	var cli *client.Client
+	{
+		c := client.Config{}
+
+		cli, err = client.New(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer cli.Connection().Close()
+	}
+
 	var aid string
 	{
 		i := &audience.CreateI{
@@ -67,8 +248,8 @@ func Test_Audience_Lifecycle(t *testing.T) {
 				Property: &audience.CreateI_Obj_Property{
 					Name: "Employees",
 					User: []string{
-						"xh3b4sd",
-						"marcoelli",
+						"foo",
+						"bar",
 					},
 				},
 			},
@@ -77,88 +258,6 @@ func Test_Audience_Lifecycle(t *testing.T) {
 		_, err := cli.Audience().Create(context.Background(), i)
 		if err == nil {
 			t.Fatal("audience name must be unique")
-		}
-	}
-
-	{
-		i := &audience.DeleteI{
-			Obj: &audience.DeleteI_Obj{
-				Metadata: map[string]string{
-					"audience.venturemark.co/id":     aid,
-					"organization.venturemark.co/id": "1",
-					"user.venturemark.co/id":         "1",
-				},
-			},
-		}
-
-		o, err := cli.Audience().Delete(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["audience.venturemark.co/status"]
-		if !ok {
-			t.Fatal("audience status must not be empty")
-		}
-
-		if s != "deleted" {
-			t.Fatal("audience status must be deleted")
-		}
-	}
-
-	{
-		i := &audience.CreateI{
-			Obj: &audience.CreateI_Obj{
-				Metadata: map[string]string{
-					"organization.venturemark.co/id": "1",
-					"user.venturemark.co/id":         "1",
-				},
-				Property: &audience.CreateI_Obj_Property{
-					Name: "Employees",
-					User: []string{
-						"xh3b4sd",
-						"marcoelli",
-					},
-				},
-			},
-		}
-
-		o, err := cli.Audience().Create(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		s, ok := o.Obj.Metadata["audience.venturemark.co/id"]
-		if !ok {
-			t.Fatal("audience ID must not be empty")
-		}
-
-		aid = s
-	}
-
-	{
-		i := &audience.SearchI{
-			Obj: []*audience.SearchI_Obj{
-				{
-					Metadata: map[string]string{
-						"organization.venturemark.co/id": "1",
-						"user.venturemark.co/id":         "1",
-					},
-				},
-			},
-		}
-
-		o, err := cli.Audience().Search(context.Background(), i)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if len(o.Obj) != 1 {
-			t.Fatal("there must be one audience")
-		}
-
-		if o.Obj[0].Property.Name != "Employees" {
-			t.Fatal("audience name must be Employees")
 		}
 	}
 
