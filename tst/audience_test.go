19b5d4c284904e0bd6e1bd6d3615a13756fc4df5
Tim Schindler <xh3b4sd@gmail.com>
2021-01-23 13:28:42 +0100

    allow audiences with zero users

--- tst/audience_test.go
+++ tst/audience_test.go
@@ -11,7 +11,9 @@ import (
 	"github.com/venturemark/fmz/pkg/client"
 )
 
-func Test_Audience_Lifecycle(t *testing.T) {
+// Test_Audience_001 ensures that the lifecycle of audiences is covered from
+// creation to deletion.
+func Test_Audience_001(t *testing.T) {
 	var err error
 
 	var cli *client.Client
@@ -192,7 +194,8 @@ func Test_Audience_Lifecycle(t *testing.T) {
 	}
 }
 
-func Test_Audience_Unique_Name(t *testing.T) {
+// Test_Audience_002 ensures that audience names are unique.
+func Test_Audience_002(t *testing.T) {
 	var err error
 
 	var cli *client.Client
@@ -259,6 +262,78 @@ func Test_Audience_Unique_Name(t *testing.T) {
 		if err == nil {
 			t.Fatal("audience name must be unique")
 		}
+	}
+
+	{
+		i := &audience.DeleteI{
+			Obj: &audience.DeleteI_Obj{
+				Metadata: map[string]string{
+					"audience.venturemark.co/id":     aid,
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+			},
+		}
+
+		o, err := cli.Audience().Delete(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/status"]
+		if !ok {
+			t.Fatal("audience status must not be empty")
+		}
+
+		if s != "deleted" {
+			t.Fatal("audience status must be deleted")
+		}
+	}
+}
+
+// Test_Audience_003 is a temporary test that ensures audiences can be created
+// with zero users.
+func Test_Audience_003(t *testing.T) {
+	var err error
+
+	var cli *client.Client
+	{
+		c := client.Config{}
+
+		cli, err = client.New(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer cli.Connection().Close()
+	}
+
+	var aid string
+	{
+		i := &audience.CreateI{
+			Obj: &audience.CreateI_Obj{
+				Metadata: map[string]string{
+					"organization.venturemark.co/id": "1",
+					"user.venturemark.co/id":         "1",
+				},
+				Property: &audience.CreateI_Obj_Property{
+					Name: "Employees",
+					User: []string{},
+				},
+			},
+		}
+
+		o, err := cli.Audience().Create(context.Background(), i)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s, ok := o.Obj.Metadata["audience.venturemark.co/id"]
+		if !ok {
+			t.Fatal("audience ID must not be empty")
+		}
+
+		aid = s
 	}
 
 	{
